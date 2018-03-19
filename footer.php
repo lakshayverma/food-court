46ba33901591c6c5bef37abab0c5f7892bc94877
Lakshay Verma <verma_lakshay@live.in>
2018-03-19 19:49:44 +0530

    Footer Updated

--- footer.php
+++ footer.php
@@ -2,43 +2,43 @@
 		<div class="container">
 			<div class="w3_footer_grids">
 				<div class="col-md-6 social-icons w3-agile-icons">
-				<h2><font color="#FFFFFF">Keep in touch</font></h2><br />  
+				<h2><font color="#FFFFFF">Keep in touch</font></h2><br />
 				<ul>
-					<li><a href="www.facebook/staple/.com" class="fa fa-facebook icon facebook"> </a></li>
-					<li><a href="www.twitter/staple/.com" class="fa fa-twitter icon twitter"> </a></li>
-					<li><a href="www.google/staple/.com" class="fa fa-google-plus icon googleplus"> </a></li>
-					<li><a href="www.dribble/staple/.com" class="fa fa-dribbble icon dribbble"> </a></li>
-					<li><a href="www.rss/staple/.com" class="fa fa-rss icon rss"> </a></li> 
-				</ul> 
+					<li><a href="#" class="fa fa-facebook icon facebook"> </a></li>
+					<li><a href="#" class="fa fa-twitter icon twitter"> </a></li>
+					<li><a href="#" class="fa fa-google-plus icon googleplus"> </a></li>
+					<li><a href="#" class="fa fa-dribbble icon dribbble"> </a></li>
+					<li><a href="#" class="fa fa-rss icon rss"> </a></li>
+				</ul>
 			</div>
-				
+
 				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
 					<h3>Pages</h3>
-					<ul>  
-						<li><a href="index.php">Home</a></li>  
+					<ul>
+						<li><a href="index.php">Home</a></li>
                         <li><a href="about.php">About Us</a></li>
-                        <li><a href="category.php">Our Menu</a></li>  
+                        <li><a href="category.php">Our Menu</a></li>
 						<li><a href="feedback.php">Feedback</a></li>
-						<li><a href="contact.php">Contact Us</a></li> 
-					</ul>   
+						<li><a href="contact.php">Contact Us</a></li>
+					</ul>
 				</div>
 				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
-					<h3>Address</h3> 
+					<h3>Address</h3>
 					<ul>
-                    <font color="#FFFFFF">
-						<li><b>Address:</b> Chhoti Baradari, Jalandhar, Punjab 144001</li> 
-						<li><b>Phone No.</b> 099884 70150</li>
-						<li><b>Opening Hours:</b>Open today · 8AM–11PM</li>
-						<li><b>Distance we Cover for  delivery:</b>100 M</a></li> 
-                      </font>
-					</ul>  
-				</div> 
+                        <font color="#FFFFFF">
+                            <li><b>Address:</b> House No 8, Jalandhar Cantt, Punjab 144005</li>
+                            <li><b>Phone No.</b> 9041972991</li>
+                            <li><b>Opening Hours:</b> 8AM–11PM</li>
+                            <li><b>Distance we Cover for delivery:</b>500 Meters</a></li>
+                        </font>
+					</ul>
+				</div>
 				<div class="clearfix"> </div>
 			</div>
-		</div> 
+		</div>
 	</div>
-	<div class="copyw3-agile"> 
+	<div class="copyw3-agile">
 		<div class="container">
-			<p>&copy; 2017 Staple Food. All rights reserved | Design by <a href="index.php">Himani & Simran</a></p>
+			<p>&copy; 2018 Sunny Side Up Bakery. All rights reserved | Developed by <a href="index.php">Sunny Verma</a></p>
 		</div>
 	</div>
