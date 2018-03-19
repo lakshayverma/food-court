f63f57b36218c0ae4674e124f6a3f7d2a7f18a8d
Lakshay Verma <verma_lakshay@live.in>
2018-03-19 22:42:57 +0530

    Fix code for Side bar

--- right.php
+++ right.php
@@ -3,33 +3,33 @@
         <div class="slider-left">
             <h4>Our Categories</h4>
             <div class="row row1 scroll-pane">
-            <ul><?php
-            include_once("connect.php");
-            $qu=mysqli_query($conn, "select * from addcategory") or die(mysqli_error($conn));
-            while ($ans=mysqli_fetch_array($qu)) {
-                ?>
+                <ul>
                 <?php
-                print "<li><a href='subcategory.php?catid=$ans[0]'>$ans[1]</a></li>"; ?></label>
-                <?php
-            }
+                include_once("connect.php");
+                $qu = mysqli_query($conn, "select * from addcategory") or die(mysqli_error($conn));
+                while ($ans=mysqli_fetch_array($qu)) {
+                    print "<li><a href='subcategory.php?catid=$ans[0]'>$ans[1]</a></li>";
+                }
                 ?>
-            </ul>
+                </ul>
             </div>
         </div>
+
         <div class="sidebar-row">
             <h4>All Cuisine</h4>
             <ul>
                 <?php
                 include_once("connect.php");
                 $qu="select * from cuisine";
-                $res=mysqli_query($conn, $qu) or die(mysqli_error($conn));
+                $res = mysqli_query($conn, $qu) or die(mysqli_error($conn));
                 while ($ans=mysqli_fetch_array($res)) {
                     print "<li><a href='product.php?cuid=$ans[0]'>$ans[1]</a></li>";
                 }
                 ?>
             </ul>
-            <div class="clearfix"> </div>
-            <div class="sidebar-row">
+        </div>
+        <div class="clearfix"> </div>
+        <div class="sidebar-row">
             <h4>All Combo</h4>
             <ul>
                 <?php
@@ -41,5 +41,6 @@
                 }
                 ?>
             </ul>
+        </div>
     </div>
 </div>
