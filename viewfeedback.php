c181fe647cae1b334a412ac6d1c6aa957e4cc924
Lakshay Verma <verma_lakshay@live.in>
2018-03-19 20:23:01 +0530

    View Feedback Pages formatted

--- viewfeedback.php
+++ viewfeedback.php
@@ -4,121 +4,121 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | Sign Up :: w3layouts</title>
+<title>Sunny Side Up Bakery  | Sign Up </title>
 <?php
 include_once("files.php");
 ?>
 </head>
-<body> 
-	<!-- banner -->
-	<div class="banner about-w3bnr">
-    	<?php
-		include_once("adminheader.php");
-		?>
-        
-	</div>
-	<!-- //banner -->    
-	<!-- breadcrumb -->  
-	<div class="container">	
-		<ol class="breadcrumb w3l-crumbs">
-			<li><a href="#"><i class="fa fa-home"></i> Home</a></li> 
-			<li class="active">view feedback</li>
-		</ol>
-	</div>
-	<!-- //breadcrumb -->
-	<!-- sign up-page -->
-	<div class="login-page about">
-		<img class="login-w3img" src="images/img3.jpg" alt="">
-		<div class="container"> 
-			<h3 class="w3ls-title w3ls-title1">View Feedback</h3>  
-			  <table width="890" height="367" border="0">
+<body>
+    <!-- banner -->
+    <div class="banner about-w3bnr">
+        <?php
+        include_once("adminheader.php");
+        ?>
+
+    </div>
+    <!-- //banner -->
+    <!-- breadcrumb -->
+    <div class="container">
+        <ol class="breadcrumb w3l-crumbs">
+            <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+            <li class="active">view feedback</li>
+        </ol>
+    </div>
+    <!-- //breadcrumb -->
+    <!-- sign up-page -->
+    <div class="login-page about">
+        <img class="login-w3img" src="images/img3.jpg" alt="">
+        <div class="container">
+            <h3 class="w3ls-title w3ls-title1">View Feedback</h3>
+              <table width="890" height="367" border="0">
             <tr>
-              		  
+
                 <?php
-	            include "connect.php";
-	  			$q="select name,email,message from feedback";
-				$r=mysqli_query($conn,$q) or die(mysqli_error($conn));
-				//$a=mysql_fetch_array($r);
-				print "<table width='416' align='center'>";
-				print "
-				<tr><th>NAME</th>
-		            <th>EMAIL</th>
-					<th>MESSAGE</th></tr>";
-		while($a=mysqli_fetch_array($r))
-		{		
-		print "<tr>";
-		print "<td>$a[0]</td>
-				<td>$a[1]</td>
-				<td>$a[2]</td>";
-		
-		print "</tr>";
-		}
-	  print "</table>";
-	  
-	  ?>              
-              </div></td>
-            </tr>
-          </table>          
-              </div></td>
-            </tr>
-          </table>
-				
-			</div>	 
-		</div>
-	</div>
-	<!-- //sign up-page -->  
-	<!-- subscribe -->
-	<?php
-	include_once("footer.php");
-	?>
-	<script src="js/minicart.js"></script>
-	<script>
+                include "connect.php";
+                  $q="select name,email,message from feedback";
+                $r=mysqli_query($conn, $q) or die(mysqli_error($conn));
+                //$a=mysql_fetch_array($r);
+                print "<table width='416' align='center'>";
+                print "
+                <tr><th>NAME</th>
+                    <th>EMAIL</th>
+                    <th>MESSAGE</th></tr>";
+                while ($a=mysqli_fetch_array($r)) {
+                    print "<tr>";
+                    print "<td>$a[0]</td>
+                        <td>$a[1]</td>
+                        <td>$a[2]</td>";
+
+                    print "</tr>";
+                }
+                print "</table>";
+                ?>
+                </div>
+                </td>
+                </tr>
+                </table>
+                    </div>
+                    </td>
+                    </tr>
+            </table>
+
+            </div>
+        </div>
+    </div>
+    <!-- //sign up-page -->
+    <!-- subscribe -->
+    <?php
+    include_once("footer.php");
+    ?>
+    <script src="js/minicart.js"></script>
+    <script>
         w3ls.render();
 
         w3ls.cart.on('w3sb_checkout', function (evt) {
-        	var items, len, i;
+            var items, len, i;
 
-        	if (this.subtotal() > 0) {
-        		items = this.items();
+            if (this.subtotal() > 0) {
+                items = this.items();
 
-        		for (i = 0, len = items.length; i < len; i++) { 
-        		}
-        	}
+                for (i = 0, len = items.length; i < len; i++) {
+                }
+            }
         });
-    </script> 
-	<!-- //cart-js -->	
-	<!-- start-smooth-scrolling -->
-	<script src="js/SmoothScroll.min.js"></script>  
-	<script type="text/javascript" src="js/move-top.js"></script>
-	<script type="text/javascript" src="js/easing.js"></script>	
-	<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-			
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-				});
-			});
-	</script>
-	<!-- //end-smooth-scrolling -->	  
-	<!-- smooth-scrolling-of-move-up -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-			/*
-			var defaults = {
-				containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-			};
-			*/
-			
-			$().UItoTop({ easingType: 'easeOutQuart' });
-			
-		});
-	</script>
-	<!-- //smooth-scrolling-of-move-up --> 
-	<!-- Bootstrap core JavaScript
+    </script>
+    <!-- //cart-js -->
+    <!-- start-smooth-scrolling -->
+    <script src="js/SmoothScroll.min.js"></script>
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $(".scroll").click(function(event){
+                    event.preventDefault();
+
+            $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                });
+            });
+    </script>
+    <!-- //end-smooth-scrolling -->
+    <!-- smooth-scrolling-of-move-up -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear'
+            };
+            */
+
+            $().UItoTop({ easingType: 'easeOutQuart' });
+
+        });
+    </script>
+    <!-- //smooth-scrolling-of-move-up -->
+    <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
