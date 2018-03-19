d5b7c0aded3c1e24a6b42f344f3d189187543fe0
Lakshay Verma <verma_lakshay@live.in>
2018-03-19 19:34:05 +0530

    Add Sub Category Page Formatted

--- addsubcat.php
+++ addsubcat.php
@@ -1,130 +1,129 @@
 <?php
-if(isset($_POST["s1"]))
-{
-	$c=$_POST["cat"];
-	$a=$_POST["name"];
-	$b=$_FILES["image"]["name"];
-	$d=$_FILES["image1"]["name"];
-	include "connect.php";
-	$query= "insert into addsubcategory(category,name,image,image1) values('$c','$a','$b','$d')";
-	$result=mysqli_query($conn,$query) or die(mysqli_error($conn));
-	mysqli_close($conn);
-	move_uploaded_file($_FILES["image"]["tmp_name"],"product/".$b);
-	move_uploaded_file($_FILES["image1"]["tmp_name"],"product/".$c);
-	$msg="Add Subcategory Successfully";
-}	
+if (isset($_POST["s1"])) {
+    $c=$_POST["cat"];
+    $a=$_POST["name"];
+    $b=$_FILES["image"]["name"];
+    $d=$_FILES["image1"]["name"];
+    include "connect.php";
+    $query= "insert into addsubcategory(category,name,image,image1) values('$c','$a','$b','$d')";
+    $result=mysqli_query($conn, $query) or die(mysqli_error($conn));
+    mysqli_close($conn);
+    move_uploaded_file($_FILES["image"]["tmp_name"], "product/".$b);
+    move_uploaded_file($_FILES["image1"]["tmp_name"], "product/".$c);
+    $msg="Add Subcategory Successfully";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | Sign Up :: w3layouts</title>
+<title>Sunny Side Up Bakery  | Sign Up </title>
 <?php
 include_once("files.php");
 ?>
 </head>
-<body> 
-	<!-- banner -->
-	<div class="banner about-w3bnr">
-    	<?php
-		include_once("adminheader.php");
-		?>
-        
-	</div>
-	<!-- //banner -->    
-	<!-- breadcrumb -->  
-	<div class="container">	
-		<ol class="breadcrumb w3l-crumbs">
-			<li><a href="#"><i class="fa fa-home"></i> Home</a></li> 
-			<li class="active">Add SubCategory</li>
-		</ol>
-	</div>
-	<!-- //breadcrumb -->
-	<!-- sign up-page -->
-	<div class="login-page about">
-		<img class="login-w3img" src="images/img3.jpg" alt="">
-		<div class="container"> 
-			<h3 class="w3ls-title w3ls-title1">Add Sub Category</h3>  
-			<div class="login-agileinfo"> 
-				<form name="abc" method="post" enctype="multipart/form-data"> 
-					<select name="cat">
-                    	<option>Choose Category</option>
+<body>
+    <!-- banner -->
+    <div class="banner about-w3bnr">
+        <?php
+        include_once("adminheader.php");
+        ?>
+
+    </div>
+    <!-- //banner -->
+    <!-- breadcrumb -->
+    <div class="container">
+        <ol class="breadcrumb w3l-crumbs">
+            <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+            <li class="active">Add SubCategory</li>
+        </ol>
+    </div>
+    <!-- //breadcrumb -->
+    <!-- sign up-page -->
+    <div class="login-page about">
+        <img class="login-w3img" src="images/img3.jpg" alt="">
+        <div class="container">
+            <h3 class="w3ls-title w3ls-title1">Add Sub Category</h3>
+            <div class="login-agileinfo">
+                <form name="abc" method="post" enctype="multipart/form-data">
+                    <select name="cat">
+                        <option>Choose Category</option>
                         <?php
-							include"connect.php";
-							$q="select id,name from addcategory";
-							$r=mysqli_query($conn,$q);
-							while($a=mysqli_fetch_array($r))
-							{
-								print"<option value='$a[0]'>$a[1]</option>";
-							}
-		  			?>
+                        include"connect.php";
+                        $q="select id,name from addcategory";
+                        $r=mysqli_query($conn, $q);
+                        while ($a=mysqli_fetch_array($r)) {
+                            print"<option value='$a[0]'>$a[1]</option>";
+                        }
+                        ?>
                     </select><br/><br/>
                     <input class="agile-ltext" type="text" name="name" placeholder="Category Name" required><br/><br/>
                     <input class="agile-ltext" type="file" name="image" required>
                     <input class="agile-ltext" type="file" name="image1" required>
-					  
-					<input type="submit" value="Add Catgeory" name="s1">
-				</form>
+
+                    <input type="submit" value="Add Catgeory" name="s1">
+                </form>
                 <?php
-					if(isset($msg))
-						print $msg;
-				?>
-				
-			</div>	 
-		</div>
-	</div>
-	<!-- //sign up-page -->  
-	<!-- subscribe -->
-	<?php
-	include_once("footer.php");
-	?>
-	<script src="js/minicart.js"></script>
-	<script>
+                if (isset($msg)) {
+                    print $msg;
+                }
+                ?>
+
+            </div>
+        </div>
+    </div>
+    <!-- //sign up-page -->
+    <!-- subscribe -->
+    <?php
+    include_once("footer.php");
+    ?>
+    <script src="js/minicart.js"></script>
+    <script>
         w3ls.render();
 
         w3ls.cart.on('w3sb_checkout', function (evt) {
-        	var items, len, i;
+            var items, len, i;
 
-        	if (this.subtotal() > 0) {
-        		items = this.items();
+            if (this.subtotal() > 0) {
+                items = this.items();
 
-        		for (i = 0, len = items.length; i < len; i++) { 
-        		}
-        	}
+                for (i = 0, len = items.length; i < len; i++) {
+                }
+            }
         });
-    </script> 
-	<!-- //cart-js -->	
-	<!-- start-smooth-scrolling -->
-	<script src="js/SmoothScroll.min.js"></script>  
-	<script type="text/javascript" src="js/move-top.js"></script>
-	<script type="text/javascript" src="js/easing.js"></script>	
-	<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-			
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-				});
-			});
-	</script>
-	<!-- //end-smooth-scrolling -->	  
-	<!-- smooth-scrolling-of-move-up -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-			/*
-			var defaults = {
-				containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-			};
-			*/
-			
-			$().UItoTop({ easingType: 'easeOutQuart' });
-			
-		});
-	</script>
-	<!-- //smooth-scrolling-of-move-up --> 
-	<!-- Bootstrap core JavaScript
+    </script>
+    <!-- //cart-js -->
+    <!-- start-smooth-scrolling -->
+    <script src="js/SmoothScroll.min.js"></script>
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $(".scroll").click(function(event){
+                    event.preventDefault();
+
+            $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                });
+            });
+    </script>
+    <!-- //end-smooth-scrolling -->
+    <!-- smooth-scrolling-of-move-up -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear'
+            };
+            */
+
+            $().UItoTop({ easingType: 'easeOutQuart' });
+
+        });
+    </script>
+    <!-- //smooth-scrolling-of-move-up -->
+    <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
