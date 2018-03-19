008ab1e3b190b11ba2515ab6789ee336929e9afe
Lakshay Verma <verma_lakshay@live.in>
2018-03-19 19:42:47 +0530

    Checkout Page Formatted

--- checkout.php
+++ checkout.php
@@ -4,30 +4,29 @@ session_start();
 ?>
 <?php
 include"connect.php";
-if(isset($_POST["next"]))
-{
-	$uname=$_SESSION["email"];
-	$tcost=$_SESSION["tcost"];
-	$fname=$_POST["fname"];
-	$lname=$_POST["lname"];
-	$email=$_POST["email"];
-	$address=$_POST["address"];
-	$city=$_POST["city"];
-	$pincode=$_POST["pincode"];
-	$phone=$_POST["phone"];	
-	$status="Pending";
-	include"connect.php";
-	$query="insert into billinginfo(fname,lname,email,address,city,pincode,phone,totalcost,uname,status,delever_by)    values('$fname','$lname','$email','$address','$city','$pincode','$phone',$tcost,'$uname','$status','none')";
-	$result=mysqli_query($conn,$query) or die(mysqli_error($conn));
-	if(mysqli_affected_rows($conn))
-	{
-		header("location:thanks.php");
-	}
-}	
-?><!DOCTYPE html>
+if (isset($_POST["next"])) {
+    $uname=$_SESSION["email"];
+    $tcost=$_SESSION["tcost"];
+    $fname=$_POST["fname"];
+    $lname=$_POST["lname"];
+    $email=$_POST["email"];
+    $address=$_POST["address"];
+    $city=$_POST["city"];
+    $pincode=$_POST["pincode"];
+    $phone=$_POST["phone"];
+    $status="Pending";
+    include"connect.php";
+    $query="insert into billinginfo(fname,lname,email,address,city,pincode,phone,totalcost,uname,status,delever_by)    values('$fname','$lname','$email','$address','$city','$pincode','$phone',$tcost,'$uname','$status','none')";
+    $result=mysqli_query($conn, $query) or die(mysqli_error($conn));
+    if (mysqli_affected_rows($conn)) {
+        header("location:thanks.php");
+    }
+}
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | Products :: w3layouts</title>
+<title>Sunny Side Up Bakery  | Products </title>
 <style type="text/css">
 .style1 {font-size: xx-large}
 .style2 {color: #EE7506}
@@ -36,27 +35,27 @@ if(isset($_POST["next"]))
 include_once("files.php");
 ?>
 </head>
-<body> 
-	<!-- banner -->
-	<div class="banner about-w3bnr">
-		<?php
-		include_once("header.php");
-		?>
-		<?php
-		include_once("banner.php");
-		?>
-	</div>
-	<!-- //banner -->    
-	<!-- breadcrumb -->  
-	<div class="container">	
-		<ol class="breadcrumb w3l-crumbs">
-			<li><a href="#"><i class="fa fa-home"></i> Home</a></li> 
-			<li class="active">Billing Info </li>
-		</ol>
-	</div>
+<body>
+    <!-- banner -->
+    <div class="banner about-w3bnr">
+        <?php
+        include_once("header.php");
+        ?>
+        <?php
+        include_once("banner.php");
+        ?>
+    </div>
+    <!-- //banner -->
+    <!-- breadcrumb -->
+    <div class="container">
+        <ol class="breadcrumb w3l-crumbs">
+            <li><a href="#"><i class="fa fa-home"></i> Home</a></li>
+            <li class="active">Billing Info </li>
+        </ol>
+    </div>
     <table width="100%" border="0" cellpadding="0">
     <tr>
-    	<td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
@@ -106,106 +105,106 @@ include_once("files.php");
           <td height="35"><input type="text" name="phone" id="phone"></td>
         </tr>
         <tr>
-          <td height="35">&nbsp;</td>
-          <td height="35"><label>
-            <input class="button-1" type="submit" name="next" id="next" value="NEXT" onClick="return abc()">
-            <?php
-          
-		  ?>
-          </label></td>
+            <td height="35">&nbsp;</td>
+            <td height="35">
+                <label>
+                    <input class="button-1" type="submit" name="next" id="next" value="NEXT" onClick="return abc()">
+                </label>
+            </td>
         </tr>
       </table>
-    </form></td>
+    </form>
+</td>
     <td width="15%">&nbsp;</td>
   </tr>
   <tr>
-    	<td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
     <tr>
-    	<td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
     <tr align="center">
-    	<td>&nbsp;</td>
+        <td>&nbsp;</td>
         <td>&nbsp;</td>
     </tr>
 </table>
-		
+
 <?php
-	include_once("footer.php");
-	?>
-	<script src="js/minicart.js"></script>
-	<script>
+    include_once("footer.php");
+    ?>
+    <script src="js/minicart.js"></script>
+    <script>
         w3ls.render();
 
         w3ls.cart.on('w3sb_checkout', function (evt) {
-        	var items, len, i;
+            var items, len, i;
 
-        	if (this.subtotal() > 0) {
-        		items = this.items();
+            if (this.subtotal() > 0) {
+                items = this.items();
 
-        		for (i = 0, len = items.length; i < len; i++) { 
-        		}
-        	}
+                for (i = 0, len = items.length; i < len; i++) {
+                }
+            }
         });
-    </script>  
-	<!-- //cart-js --> 
-	<!-- Owl-Carousel-JavaScript -->
-	<script src="js/owl.carousel.js"></script>
-	<script>
-		$(document).ready(function() {
-			$("#owl-demo").owlCarousel ({
-				items : 3,
-				lazyLoad : true,
-				autoPlay : true,
-				pagination : true,
-			});
-		});
-	</script>
-	<!-- //Owl-Carousel-JavaScript -->  	
-	<!-- the jScrollPane script -->
-	<script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
-	<script type="text/javascript" id="sourcecode">
-		$(function()
-		{
-			$('.scroll-pane').jScrollPane();
-		});
-	</script>
-	<!-- //the jScrollPane script -->
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script> <!-- the mouse wheel plugin --> 
-	<!-- start-smooth-scrolling -->
-	<script src="js/SmoothScroll.min.js"></script>  
-	<script type="text/javascript" src="js/move-top.js"></script>
-	<script type="text/javascript" src="js/easing.js"></script>	
-	<script type="text/javascript">
-			jQuery(document).ready(function($) {
-				$(".scroll").click(function(event){		
-					event.preventDefault();
-			
-			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-				});
-			});
-	</script>
-	<!-- //end-smooth-scrolling -->	  
-	<!-- smooth-scrolling-of-move-up -->
-	<script type="text/javascript">
-		$(document).ready(function() {
-			/*
-			var defaults = {
-				containerID: 'toTop', // fading element id
-				containerHoverID: 'toTopHover', // fading element hover id
-				scrollSpeed: 1200,
-				easingType: 'linear' 
-			};
-			*/
-			
-			$().UItoTop({ easingType: 'easeOutQuart' });
-			
-		});
-	</script>
-	<!-- //smooth-scrolling-of-move-up -->  
-	<!-- Bootstrap core JavaScript
+    </script>
+    <!-- //cart-js -->
+    <!-- Owl-Carousel-JavaScript -->
+    <script src="js/owl.carousel.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#owl-demo").owlCarousel ({
+                items : 3,
+                lazyLoad : true,
+                autoPlay : true,
+                pagination : true,
+            });
+        });
+    </script>
+    <!-- //Owl-Carousel-JavaScript -->
+    <!-- the jScrollPane script -->
+    <script type="text/javascript" src="js/jquery.jscrollpane.min.js"></script>
+    <script type="text/javascript" id="sourcecode">
+        $(function()
+        {
+            $('.scroll-pane').jScrollPane();
+        });
+    </script>
+    <!-- //the jScrollPane script -->
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script> <!-- the mouse wheel plugin -->
+    <!-- start-smooth-scrolling -->
+    <script src="js/SmoothScroll.min.js"></script>
+    <script type="text/javascript" src="js/move-top.js"></script>
+    <script type="text/javascript" src="js/easing.js"></script>
+    <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $(".scroll").click(function(event){
+                    event.preventDefault();
+
+            $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                });
+            });
+    </script>
+    <!-- //end-smooth-scrolling -->
+    <!-- smooth-scrolling-of-move-up -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear'
+            };
+            */
+
+            $().UItoTop({ easingType: 'easeOutQuart' });
+
+        });
+    </script>
+    <!-- //smooth-scrolling-of-move-up -->
+    <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.js"></script>
